bc69cdd535beb1d7b0632f0433f47c4e475b0461
apocsenpai <jonatassennas@ufrrj.br>
2022-11-03 17:11:00 -0300

    feat(quizPage): create a exibition page of selected quizz

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ const page2 = document.querySelector('.quizz-page-screen');
 
 const url = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
 const allList = document.querySelector('.all');
-
+const quizzPage = document.querySelector('.quizz-page');
 
 /*Navegação pagina1 -> pagina3*/
 btnCreat.addEventListener('click', () => {
@@ -36,6 +36,7 @@ function renderAllQuizz(list) {
         elm.addEventListener('click', () => {
             page1.classList.toggle('hidden');
             page2.classList.toggle('hidden');
+            getSingleQuizz(elm);
         })
     })
 }
@@ -51,4 +52,60 @@ function getAllQuizz() {
 
 getAllQuizz();
 
+/*Função que busca o quizz clicado selecionado*/
 
+function getSingleQuizz(elm){
+    const quizzId = elm.id;
+    const promise = axios.get(`${url}quizzes/${elm.id}`);
+    promise.then(response =>{
+        const data = response.data;
+        renderSingleQuizz(data);
+    })
+    promise.catch(error => console.log(error));
+}
+
+function renderSingleQuizz(quizz){
+    quizzPage.innerHTML = `
+        <header class="quizz-title">
+            <h3>${quizz.title}</h3>
+        </header>
+    `;
+    const quizzTitle = document.querySelector('.quizz-title');
+    quizzTitle.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${quizz.image})`;
+
+    quizz.questions.forEach(singleQuestion =>{
+
+        let questionAnswers = '';
+        let answersList = singleQuestion.answers.sort(randomizeAnswers);
+
+        answersList.forEach(answers=>{
+            questionAnswers += `
+            <div class="quizz-single-answer">
+                <img
+                src="${answers.image}"
+                alt=""
+                />
+                <h5>${answers.text}</h5>
+            </div>
+            `;
+        });
+
+        quizzPage.innerHTML += `
+        <section class="quizz-content">
+              <header class="quizz-question" style="background-color:${singleQuestion.color}">
+                <h4>${singleQuestion.title}</h4>
+              </header>
+              <main class="quizz-answers">
+                ${questionAnswers}
+              </main>
+        </section>
+        `;
+        
+        
+
+    });
+}
+
+function randomizeAnswers(){
+    return Math.random() - 0.5;
+}
