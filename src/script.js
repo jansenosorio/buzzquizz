efba36d560c11249e544c579c81033857e3d848f
JeffMota <jmota906@gmail.com>
2022-11-03 10:16:23 -0300

    Navegation - Quizz click -> Quizz page

--- src/script.js
+++ src/script.js
@@ -1,17 +1,22 @@
 const noQuizz = document.querySelector('.no-quizz');
 const withQuizz = document.querySelector('.with-quizz');
 
+const btnCreat = document.querySelector('.btn-create');
+
 /*Paginas para navegação com 'hidden'*/
 const page3 = document.querySelector('.container-page-3-1');
 const page1 = document.querySelector('.quizz-list-screen');
+const page2 = document.querySelector('.quizz-page-screen');
 
 const url = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
 const allList = document.querySelector('.all');
 
-function toggleTest(){
+
+/*Navegação pagina1 -> pagina3*/
+btnCreat.addEventListener('click', () => {
     page1.classList.toggle('hidden');
     page3.classList.toggle('hidden');
-}
+})
 
 /*Função para renderizar os quizzes de outros usuarios*/
 function renderAllQuizz(list) {
@@ -24,6 +29,15 @@ function renderAllQuizz(list) {
         </div>
         `
     });
+    
+    //Navegação page1 -> page2
+    const addListen = document.querySelectorAll('.quizz');
+    addListen.forEach(elm => {
+        elm.addEventListener('click', () => {
+            page1.classList.toggle('hidden');
+            page2.classList.toggle('hidden');
+        })
+    })
 }
 /*Função quer busca o quiz de outros usuarios*/
 function getAllQuizz() {
