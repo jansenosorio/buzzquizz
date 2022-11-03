01ca5620389b644089a412d56d9489cf2d73f106
JeffMota <jmota906@gmail.com>
2022-11-03 09:45:01 -0300

    List page - Get and list all other users quizz

--- src/script.js
+++ src/script.js
@@ -1,7 +1,36 @@
 const noQuizz = document.querySelector('.no-quizz');
 const withQuizz = document.querySelector('.with-quizz');
 
+const url = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
+const allList = document.querySelector('.all');
+
 function toggleTest(){
     noQuizz.classList.toggle('hidden');
     withQuizz.classList.toggle('hidden');
 }
+
+/*Função para renderizar os quizzes de outros usuarios*/
+function renderAllQuizz(list) {
+    list.forEach(elm => {
+        allList.innerHTML += `
+        <div id="${elm.id}" style="background-image: url(${elm.image})" class="quizz">
+            <h3>
+            ${elm.title}
+            </h3>
+        </div>
+        `
+    });
+}
+/*Função quer busca o quiz de outros usuarios*/
+function getAllQuizz() {
+    const promise = axios.get(url + "quizzes")
+    promise.then(response => {
+        const data = response.data;
+        renderAllQuizz(data);
+    })
+    promise.catch(error => console.log(error));
+}
+
+getAllQuizz();
+
+
