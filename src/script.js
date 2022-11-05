b312f9f141652041c386f3096a72f141ad3cde04
jansenosorio <dev.jansen93@gmail.com>
2022-11-04 22:45:22 -0300

    feat: Created validation input data and render next page of create questions

--- src/script.js
+++ src/script.js
@@ -1,99 +1,99 @@
 // const noQuizz = document.querySelector('.no-quizz');
 // const withQuizz = document.querySelector('.with-quizz');
 
-const btnCreat = document.querySelectorAll('.btn-create');
+const btnCreat = document.querySelectorAll('.btn-create')
 
 /*Paginas para navegação com 'hidden'*/
-const page3 = document.querySelector('.container-page-3-1');
-const page1 = document.querySelector('.quizz-list-screen');
-const page2 = document.querySelector('.quizz-page-screen');
+const page3 = document.querySelector('.container-page-3-1')
+const page1 = document.querySelector('.quizz-list-screen')
+const page2 = document.querySelector('.quizz-page-screen')
 
-const url = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
-const allList = document.querySelector('.all');
-const quizzPage = document.querySelector('.quizz-page');
+const url = 'https://mock-api.driven.com.br/api/v4/buzzquizz/'
+const allList = document.querySelector('.all')
+const quizzPage = document.querySelector('.quizz-page')
 
 /*Lista com todos os quizzes*/
-let quizzList;
-
+let quizzList
 
 /*Navegação pagina1 -> pagina3*/
 btnCreat.forEach(elm => {
-    elm.addEventListener('click', () => {
-        page1.classList.toggle('hidden');
-        page3.classList.toggle('hidden');
-    })
+  elm.addEventListener('click', () => {
+    page1.classList.toggle('hidden')
+    page3.classList.toggle('hidden')
+  })
 })
 
 /*Função para renderizar os quizzes de outros usuarios*/
 function renderAllQuizz(list) {
-    let index = [];
-    while (index.length < 9) {
-        let aux = Math.floor(Math.random() * list.length);
-        if (!index.includes(aux)) {
-            index.push(aux);
-        }
+  let index = []
+  while (index.length < 9) {
+    let aux = Math.floor(Math.random() * list.length)
+    if (!index.includes(aux)) {
+      index.push(aux)
     }
+  }
 
-    for (let i = 0; i < index.length; i++) {
-            allList.innerHTML += `
-            <div id="${list[index[i]].id}" style="background-image: url(${list[index[i]].image})" class="quizz">
+  for (let i = 0; i < index.length; i++) {
+    allList.innerHTML += `
+            <div id="${list[index[i]].id}" style="background-image: url(${
+      list[index[i]].image
+    })" class="quizz">
                 <h3>
                 ${list[index[i]].title}
                 </h3>
             </div>
         `
-    }
+  }
 
-    //Navegação page1 -> page2
-    const addListen = document.querySelectorAll('.quizz');
-    addListen.forEach(elm => {
-        elm.addEventListener('click', () => {
-            page1.classList.toggle('hidden');
-            page2.classList.toggle('hidden');
-            getSingleQuizz(elm);
-        })
+  //Navegação page1 -> page2
+  const addListen = document.querySelectorAll('.quizz')
+  addListen.forEach(elm => {
+    elm.addEventListener('click', () => {
+      page1.classList.toggle('hidden')
+      page2.classList.toggle('hidden')
+      getSingleQuizz(elm)
     })
+  })
 }
 /*Função quer busca o quiz de outros usuarios*/
 function getAllQuizz() {
-    const promise = axios.get(url + "quizzes")
-    promise.then(response => {
-        quizzList = response.data;
-        renderAllQuizz(quizzList);
-    })
-    promise.catch(error => console.log(error));
+  const promise = axios.get(url + 'quizzes')
+  promise.then(response => {
+    quizzList = response.data
+    renderAllQuizz(quizzList)
+  })
+  promise.catch(error => console.log(error))
 }
 
-getAllQuizz();
+getAllQuizz()
 
 /*Função que busca o quizz clicado selecionado*/
 
-function getSingleQuizz(elm){
-    const quizzId = elm.id;
-    const promise = axios.get(`${url}quizzes/${elm.id}`);
-    promise.then(response =>{
-        const data = response.data;
-        renderSingleQuizz(data);
-    })
-    promise.catch(error => console.log(error));
+function getSingleQuizz(elm) {
+  const quizzId = elm.id
+  const promise = axios.get(`${url}quizzes/${elm.id}`)
+  promise.then(response => {
+    const data = response.data
+    renderSingleQuizz(data)
+  })
+  promise.catch(error => console.log(error))
 }
 
-function renderSingleQuizz(quizz){
-    quizzPage.innerHTML = `
+function renderSingleQuizz(quizz) {
+  quizzPage.innerHTML = `
         <header class="quizz-title">
             <h3>${quizz.title}</h3>
         </header>
-    `;
-    const quizzTitle = document.querySelector('.quizz-title');
-    quizzTitle.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${quizz.image})`;
+    `
+  const quizzTitle = document.querySelector('.quizz-title')
+  quizzTitle.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${quizz.image})`
 
-    quizz.questions.forEach(singleQuestion =>{
+  quizz.questions.forEach(singleQuestion => {
+    let questionAnswers = ''
+    let answersList = singleQuestion.answers.sort(randomizeAnswers)
 
-        let questionAnswers = '';
-        let answersList = singleQuestion.answers.sort(randomizeAnswers);
-
-        answersList.forEach(answers=>{
-            questionAnswers += `
+    answersList.forEach(answers => {
+      questionAnswers += `
             <div class="quizz-single-answer">
                 <img
                 src="${answers.image}"
@@ -101,10 +101,10 @@ function renderSingleQuizz(quizz){
                 />
                 <h5>${answers.text}</h5>
             </div>
-            `;
-        });
+            `
+    })
 
-        quizzPage.innerHTML += `
+    quizzPage.innerHTML += `
         <section class="quizz-content">
               <header class="quizz-question" style="background-color:${singleQuestion.color}">
                 <h4>${singleQuestion.title}</h4>
@@ -113,13 +113,10 @@ function renderSingleQuizz(quizz){
                 ${questionAnswers}
               </main>
         </section>
-        `;
-        
-        
-
-    });
+        `
+  })
 }
 
-function randomizeAnswers(){
-    return Math.random() - 0.5;
+function randomizeAnswers() {
+  return Math.random() - 0.5
 }
