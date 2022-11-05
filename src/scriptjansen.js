2feaa2d1c4bf0fa4b09af2221620bdc9ca96cc89
jansenosorio <dev.jansen93@gmail.com>
2022-11-05 16:44:22 -0300

    feat: Completed page 3.2, with all verification functions and error messages

--- src/scriptjansen.js
+++ src/scriptjansen.js
@@ -8,6 +8,11 @@ let btCreateQuestions = ''
 let containerScreen31 = ''
 let containerScreen32 = ''
 let creatYourQuestions = ''
+let textQuestion = ''
+let hexadecimal = ''
+let inUrl32 = ''
+let correctAnswer = ''
+let incorrectAnswer = ''
 
 function getElementsFromHtml() {
   // get elements from html
@@ -22,18 +27,23 @@ function getElementsFromHtml() {
     document.querySelector('.in-number-questions').value
   )
   inNumberLevels = Number(document.querySelector('.in-number-levels').value)
+  textQuestion = document.querySelectorAll('.in-title-question')
+  hexadecimal = document.querySelectorAll('.in-background-question')
+  correctAnswer = document.querySelectorAll('.in-correct-answer')
+  incorrectAnswer = document.querySelectorAll('.in-incorrect-answer')
+  inUrl32 = document.querySelectorAll('.in-url-img')
 
   // Get buttons
   btCreateQuestions = document.querySelector('.bt-create-questions')
 
   // Add EventListener to Button
-  btCreateQuestions.addEventListener('click', validationIputData)
+  btCreateQuestions.addEventListener('click', validationIputData31)
 }
 
 getElementsFromHtml()
 
-// Function to validation input data, if it is ok, go to next page
-function validationIputData() {
+// Function to validation input data page 3.1, if it is ok, go to next page
+function validationIputData31() {
   getElementsFromHtml()
 
   if (
@@ -47,6 +57,22 @@ function validationIputData() {
     renderNextPage(inNumberQuestions)
   } else {
     alert('Favor verificar os campos abaixo')
+  }
+}
+
+function validationIputData32() {
+  getElementsFromHtml()
+
+  if (
+    textQuestionValidation(textQuestion) === true &&
+    isHexadecimalColor(hexadecimal) === true &&
+    isUrl32(inUrl32) === true &&
+    isCorrectAnswerEmpty(correctAnswer) === true &&
+    isIncorrectAnswerEmpty(incorrectAnswer) === true
+  ) {
+    console.log('tudo certo')
+  } else {
+    console.log('algo não está certo')
   }
 }
 
@@ -87,6 +113,92 @@ function isValidNumberOfLevels(levels) {
   }
 }
 
+// Function to validation input data page 3.2, if it is ok, go to next page
+function textQuestionValidation(textQuestion) {
+  let count = ''
+  textQuestion.forEach(elm => {
+    if (elm.value !== '' && elm.value.length > 20) {
+      count++
+    }
+  })
+
+  if (textQuestion.length === count) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// Function to validation input data Hexadecimal color
+function isHexadecimalColor(hexadecimal) {
+  let str = new RegExp('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
+  let count = ''
+
+  hexadecimal.forEach(elm => {
+    if (elm.value !== '' && str.test(elm.value) == true) {
+      count++
+    }
+  })
+
+  if (hexadecimal.length == count) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isCorrectAnswerEmpty(answer) {
+  let count = ''
+  answer.forEach(elm => {
+    if (elm.value != '') {
+      count++
+    }
+  })
+
+  if (answer.length == count) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isIncorrectAnswerEmpty(incorrectAnswer) {
+  let count = ''
+  incorrectAnswer.forEach(elm => {
+    if (elm.value != '') {
+      count++
+    }
+  })
+
+  if (count >= inNumberQuestions) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isUrl32(url32) {
+  let count = ''
+  let filledIput = ''
+  url32.forEach(elm => {
+    if (elm.value !== '') {
+      filledIput++
+      try {
+        let url = new URL(elm.value)
+        count++
+      } catch (err) {
+        console.log(elm.value)
+      }
+    }
+  })
+
+  if (count === filledIput) {
+    return true
+  } else {
+    return false
+  }
+}
+
 // Function to render the next page
 
 function renderNextPage(numberQuestions) {
@@ -102,12 +214,12 @@ function renderNextPage(numberQuestions) {
           <h2 class="title-question">Pergunta ${i}</h2>
           <input
             type="text"
-            class="in-title-question${i}"
+            class="in-title-question"
             placeholder="Texto da pergunta"
           />
           <input
             type="text"
-            class="in-background-question1${i}"
+            class="in-background-question"
             placeholder="Cor de fundo da pergunta"
           />
         </div>
@@ -115,12 +227,13 @@ function renderNextPage(numberQuestions) {
           <h2 class="title-question">Resposta correta</h2>
           <input
             type="text"
-            class="in-correct-answer1${i}"
+            class="in-correct-answer"
             placeholder="Resposta correta"
           />
           <input
             type="text"
-            class="in-url-correct-img1${i}"
+            class="in-url-img"
+            data-img = 'url-correct-img'
             placeholder="URL da imagem"
           />
         </div>
@@ -128,36 +241,42 @@ function renderNextPage(numberQuestions) {
           <h2 class="title-question">Resposta incorreta</h2>
           <input
             type="text"
-            class="in-incorrect-answer-1${i}"
+            class="in-incorrect-answer"
+            data-incorrect-answer = "1"
             placeholder="Resposta incorreta 1"
           />
           <input
             type="text"
-            class="in-url-incorrect-img-1${i}"
+            class="in-url-img"
+            data-img = 'url-incorrect-img-1'
             placeholder="URL da imagem 1"
           />
         </div>
         <div class="box-questions-without-title">
           <input
             type="text"
-            class="in-incorrect-answer-2${i}"
+            class="in-incorrect-answer"
+            data-incorrect-answer = "2"
             placeholder="Resposta incorreta 2"
           />
           <input
             type="text"
-            class="in-url-incorrect-img-2${i}"
+            class="in-url-img"
+            data-img = 'url-incorrect-img-2'
             placeholder="URL da imagem 2"
           />
         </div>
         <div class="box-questions-without-title">
           <input
             type="text"
-            class="in-incorrect-answer-3${i}"
+            class="in-incorrect-answer"
+            data-incorrect-answer = "3"
             placeholder="Resposta incorreta 3"
           />
           <input
             type="text"
-            class="in-url-incorrect-img-3${i}"
+            class="in-url-img"
+            data-img = 'url-incorrect-img-3'
             placeholder="URL da imagem 3"
           />
         </div>
@@ -168,6 +287,8 @@ function renderNextPage(numberQuestions) {
 
   setEventListenerToIconEdit()
 }
+
+// Function to hidden and show selected inputs
 
 function hiddenAndShowCreateQuestions({ target }) {
   let containerFatherQuestions = document.querySelectorAll(
